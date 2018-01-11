d3ef100020e217eb8922e7ae351df6b36a812792
weimanqing <weimanqing561@163.com>
2018-01-11 09:51:38 +0800

    smartOffice 起始
    smartOffice 起始

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
